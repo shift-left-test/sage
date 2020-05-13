ff6c06c50543ea72c74c88d521a43ac8e9b0e652
Daeseong Seong <daeseong.seong@lgepartner.com>
2020-05-13 17:03:56 +0900

    add sample_project to verify

--- test_env_gen.sh
+++ test_env_gen.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -eE
 
-mkdir -p tmp
-cd tmp
-
-if [ ! -d "$1" ]; then
-    git clone http://mod.lge.com/hub/yocto/sample/$1.git
-    cd $1
-else
-    cd $1
-    git pull
-fi
-
-mkdir -p build && cd build
-qmake ..
-cd ../..
+cd sample_project
+mkdir -p build
+cd build
+cmake ..
