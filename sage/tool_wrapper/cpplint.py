7aee786f12210999db0655582af9da819a695b54
Daeseong Seong <daeseong.seong@lgepartner.com>
2020-05-14 16:13:28 +0900

    change cpplint output format to vs7

--- sage/tool_wrapper/cpplint.py
+++ sage/tool_wrapper/cpplint.py
@@ -8,8 +8,8 @@ class CppLintWrapper(ToolWrapper):
         args = ["cpplint"]
         REPORT = None
         if self.ctx.output_path:
-            args += ["--output=junit"]
-            REPORT = open(os.path.join(self.ctx.output_path, "cpplint_report.xml"), "w")
+            args += ["--output=vs7"]
+            REPORT = open(os.path.join(self.ctx.output_path, "cpplint_report.txt"), "w")
         args += self.get_src_list()
         os.chdir(self.ctx.src_path)
         subprocess.call(args, stderr=REPORT)
