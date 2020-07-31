2e735adfac5b410a2413ee077614051f548da6aa
Daeseong Seong <daeseong.seong@lgepartner.com>
2020-07-31 11:05:40 +0900

    add invalid tool-path & build-path test case

--- tests/test_sage_executable.py
+++ tests/test_sage_executable.py
@@ -9,7 +9,8 @@ def test_basic(basic_build):
         "--source-path",
         basic_build.src_path,
         "--build-path",
-        basic_build.bld_path],
+        basic_build.bld_path,
+        "--verbose"],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE)
 
@@ -26,22 +27,57 @@ def test_output(basic_build):
         "--build-path",
         basic_build.bld_path,
         "--output-path",
-        "{}/report".format(basic_build.bld_path)
+        "{}/report".format(basic_build.bld_path),
+        "--verbose"
         ],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE)
     
     (output, error) = proc.communicate()
-    print(output)
-    print(error)
+
     assert os.path.exists("{}/report/cpplint_report.txt".format(basic_build.bld_path))
+
+
+def test_invalid_tool_path(basic_build):
+    proc = output = subprocess.Popen([
+        "sage",
+        "--source-path",
+        basic_build.src_path,
+        "--build-path",
+        basic_build.bld_path,
+        "--tool-path",
+        "/bin",
+        "--verbose"],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE)
+
+    (output, error) = proc.communicate()
+
+    assert u"* cpplint is not installed" in str(output)
+
+
+def test_invalid_build_path(basic_build):
+    proc = output = subprocess.Popen([
+        "sage",
+        "--source-path",
+        basic_build.src_path,
+        "--build-path",
+        os.path.join(basic_build.bld_path, "tmp"),
+        "--verbose"],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE)
+
+    (output, error) = proc.communicate()
+
+    assert u"There is no 'compile_commands.json'" in str(output)
 
 
 def test_only_source(source_only_build):
     proc = output = subprocess.Popen([
         "sage",
         "--source-path",
-        source_only_build.src_path],
+        source_only_build.src_path,
+        "--verbose"],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE)
 
