6172a90c741f62cd8af9369bbe59a7983f0d9d83
sangmo.kang <sangmo.kang@lge.com>
2021-12-23 17:27:39 +0900

    #57 add tests for clang-tidy

--- tests/test_module_check_clang_tidy.py
+++ tests/test_module_check_clang_tidy.py
@@ -69,3 +69,67 @@ def test_run_clangtidy_no_issue(basic_build):
 def test_run_clangtidy_no_src(empty_build):
     ctx = empty_build.run_tools(["clang-tidy"])
     assert len(ctx.file_analysis_map) == 0
+
+
+def test_run_clangtidy_severity_error(basic_build):
+    # "asdf.h" file not found
+    MAIN_CONTENT = """
+#include "asdf.h"
+
+int main() {
+    return 0;
+}
+"""
+    basic_build.add_src_file("main.cpp", MAIN_CONTENT)
+    ctx = basic_build.run_tools(["clang-tidy"])
+
+    assert len(ctx.file_analysis_map) != 0
+
+    for file_name, file_analysis in ctx.file_analysis_map.items():
+        assert get_num_of_issues(file_analysis) == 1
+        issues = get_major_issues(file_analysis)
+        assert len(issues) == 1
+        assert issues[0].severity == 'error'
+
+
+def test_run_clangtidy_severity_warning(basic_build):
+    # implicit conversion from 'double' to 'int' changes value
+    MAIN_CONTENT = """
+int main() {
+    return 1.1;
+}
+"""
+    basic_build.add_src_file("main.cpp", MAIN_CONTENT)
+    ctx = basic_build.run_tools(["clang-tidy"])
+
+    assert len(ctx.file_analysis_map) != 0
+
+    for file_name, file_analysis in ctx.file_analysis_map.items():
+        assert get_num_of_issues(file_analysis) == 1
+        issues = get_minor_issues(file_analysis)
+        assert len(issues) == 1
+        assert issues[0].severity == 'warning'
+
+
+def test_run_clangtidy_severity_note(basic_build):
+    # warning: Potential leak of memory pointed to by 'p'
+    # note: Memory is allocated
+    # note: Potential leak of memory pointed to by 'p'
+    MAIN_CONTENT = """
+int main() {
+    int *p = new int[1];
+    p[0] = 1;
+    return 0;
+}
+"""
+    basic_build.add_src_file("main.cpp", MAIN_CONTENT)
+    ctx = basic_build.run_tools(["clang-tidy"])
+
+    assert len(ctx.file_analysis_map) != 0
+
+    for file_name, file_analysis in ctx.file_analysis_map.items():
+        assert get_num_of_issues(file_analysis) != 0
+        issues = get_info_issues(file_analysis)
+        assert len(issues) != 0
+        assert issues[0].severity == 'note'
+
